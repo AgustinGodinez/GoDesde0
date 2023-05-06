910182cea6e9da5c7593e69197d88171a4dcf032
AgustinGodinez <105577729+AgustinGodinez@users.noreply.github.com>
2023-05-05 20:14:13 -0600

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AgustinGodinez/GoDesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/AgustinGodinez/GoDesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
